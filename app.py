7d6489cec2fc9c834c7e9b6030f4d028bc3d6f93
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-10-26 19:13:34 +0200

    Added start page

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from whatsdown import app
+from flask import render_template
+import vlc
 
 
 @app.route('/')
 def home_page():
-    return 'Hello doge'
+    sad_song = vlc.MediaPlayer('whatsdown/media/chopin_funeral.mp3')
+    sad_song.play()
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
--- whatsdown/__init__.py
+++ whatsdown/__init__.py
@@ -8,14 +8,19 @@ from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 
+# db & admin config
 app.secret_key = 'w4lepsze'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@127.0.0.1/{}'.format(name, password, db_name)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 db = SQLAlchemy(app)
 Migrate(app, db)
+admin = flask_admin.Admin(app)
+
+
+# models have to be imported after db creation
 from .models import Administrator
 
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-admin = flask_admin.Admin(app)
+# here models are registered to admin
 admin.add_view(ModelView(Administrator, db.session))
