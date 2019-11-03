4f253f7816e1d5344fd709ccbfe586ef59f0bdee
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-11-03 08:29:45 +0100

    Formatted code in pycharm way

--- whatsdown/models.py
+++ whatsdown/models.py
@@ -4,28 +4,26 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 from datetime import date, datetime
 
+
 # czy 'data' to to samo co "data"?
 # w szczególności przy oznaczaniu rodziców i dzieci w kluczach obcych ('parent) vs ("parent")
 
 
 class Administrator(db.Model, UserMixin):
     __tablename__ = 'administrator'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     login = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(30), nullable=False)
-    
+
     def __repr__(self):
-        return 'Administrator %d, login %s' % (format(self.id), format(self.login) )
-       
+        return 'Administrator %d, login %s' % (format(self.id), format(self.login))
 
 
 class User(db.Model, UserMixin):
     # connected with funeral o-t-m
     # atributes
     __tablename__ = 'funeral_home'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(30), unique=True)
     voivodeship = db.Column(db.Text)
     county = db.Column(db.Text)
@@ -41,20 +39,20 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return 'Dom pogrzebowy {}'.format(self.name)
 
+
 class Tombstone(db.Model):  # connected with quarter o-t-m
     # atributes
     __tablename__ = 'tombstone'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     manufacturer = db.Column(db.Text)
     material = db.Column(db.Text)
     price = db.Column(db.Integer)
 
-    #relationships
+    # relationships
     quarter = relationship("Quarter", back_populates="tombstone")
 
     def __repr__(self):
-        return 'Nagrobek marki %s, model %s' % (format(self.manufacturer), format(self.material) )
+        return 'Nagrobek marki %s, model %s' % (format(self.manufacturer), format(self.material))
 
 
 class Quarter(db.Model):
@@ -63,32 +61,29 @@ class Quarter(db.Model):
     # connected with buried m-t-o
     # atributes
     __tablename__ = 'quarter'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     x_coord = db.Column(db.Integer)
     y_coord = db.Column(db.Integer)
     price = db.Column(db.Integer)
 
     # foreign keys
-    cemetery_id = db.Column(db.Integer, db.ForeignKey(
-        'cemetery.id'), nullable=False)
-    tombstone_id = db.Column(
-        db.Integer, db.ForeignKey('tombstone.id'), nullable=True)
+    cemetery_id = db.Column(db.Integer, db.ForeignKey('cemetery.id'), nullable=False)
+    tombstone_id = db.Column(db.Integer, db.ForeignKey('tombstone.id'), nullable=True)
 
-    #relationships
+    # relationships
     cemetery = relationship("Cemetery", back_populates="quarters")
     buried = relationship("Buried", back_populates="quarter")
     tombstone = relationship("Tombstone", back_populates="quarter")
 
     def __repr__(self):
-        return 'Kwatera na cmentarzu nr %d, x %d, y %d' % (self.cemetery_id, self.x_coord, self.y_coord )
+        return 'Kwatera na cmentarzu nr %d, x %d, y %d' % (self.cemetery_id, self.x_coord, self.y_coord)
+
 
 class Cemetery(db.Model):
     # connected with quarter m-t-o
     # atributes
     __tablename__ = 'cemetery'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     voivodeship = db.Column(db.Text)
     county = db.Column(db.Text)
     locality = db.Column(db.Text)
@@ -99,44 +94,46 @@ class Cemetery(db.Model):
     quarters = relationship("Quarter", back_populates="cemetery")
 
     def __repr__(self):
-        return 'Cmentarz w woj. %s, powiat %s, miejscowość %s, ulica %s ' % (format(self.voivodeship), format(self.county), format(self.locality), format(self.street) )
+        return 'Cmentarz w woj. %s, powiat %s, miejscowość %s, ulica %s ' % (
+            format(self.voivodeship), format(self.county), format(self.locality), format(self.street))
+
 
 class Outfit(db.Model):
     # connected with buried m-t-o
 
     # atributes
     __tablename__ = 'outfit'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     type_of_clothing = db.Column(db.Text)
     size = db.Column(db.Text)
     brand = db.Column(db.Text)
     color = db.Column(db.Text)
     price = db.Column(db.Integer)
 
-    #relationships
+    # relationships
     buried = relationship("Buried", back_populates="outfit")
 
     def __repr__(self):
-        return '%s, marki  %s, rozmiar %s, w kolorze %s' % (format(self.type_of_clothing), format(self.brand), format(self.size), format(self.color) )
+        return '%s, marki  %s, rozmiar %s, w kolorze %s' % (
+            format(self.type_of_clothing), format(self.brand), format(self.size), format(self.color))
+
 
 class Container(db.Model):
     # connected with buried m-t-o
 
     # atributes
     __tablename__ = 'container'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     type_of_container = db.Column(db.Text)
     manufacturer = db.Column(db.Text)
     material = db.Column(db.Text)
     price = db.Column(db.Integer)
 
-    #relationships
+    # relationships
     buried = relationship("Buried", back_populates="container")
 
     def __repr__(self):
-        return 'Pojemnik %s, wykonany z %s' % (format(self.manufacturer), format(self.material) )
+        return 'Pojemnik %s, wykonany z %s' % (format(self.manufacturer), format(self.material))
 
 
 class Buried(db.Model):
@@ -146,8 +143,7 @@ class Buried(db.Model):
 
     # atributes
     __tablename__ = 'buried'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     first_name = db.Column(db.Text)
     last_name = db.Column(db.Text)
     birth_date = db.Column(db.DateTime())
@@ -156,23 +152,19 @@ class Buried(db.Model):
 
     # foreign keys
     outfit_id = db.Column(db.Integer, ForeignKey('outfit.id'), nullable=True)
-    container_id = db.Column(
-        db.Integer, ForeignKey('container.id'), nullable=True)
-    quarter_id = db.Column(db.Integer, ForeignKey(
-        'quarter.id'), nullable=False)
-    funeral_id = db.Column(db.Integer, ForeignKey(
-        'funeral.id'), nullable=False)
+    container_id = db.Column(db.Integer, ForeignKey('container.id'), nullable=True)
+    quarter_id = db.Column(db.Integer, ForeignKey('quarter.id'), nullable=False)
+    funeral_id = db.Column(db.Integer, ForeignKey('funeral.id'), nullable=False)
 
     # bidirectional relationships
     quarter = relationship("Quarter", back_populates="buried")
     funeral = relationship("Funeral", back_populates="buried")
     container = relationship("Container", back_populates="buried")
     outfit = relationship("Outfit", back_populates="buried")
-    
 
     def __repr__(self):
-        return 'Pochowany %s %s' % (format(self.first_name), format(self.last_name) )
-       
+        return 'Pochowany %s %s' % (format(self.first_name), format(self.last_name))
+
 
 class Funeral(db.Model):
     # connected with funeral_home o-t-m
@@ -181,8 +173,7 @@ class Funeral(db.Model):
 
     # atributes
     __tablename__ = 'funeral'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     date = db.Column(db.DateTime(), default=datetime.utcnow)
     total_price = db.Column(db.Integer)
 
@@ -196,15 +187,11 @@ class Funeral(db.Model):
 
     def __repr__(self):
         return 'Pogrzeb nr {}'.format(self.id)
-       
 
-# table many-to-many
-priest_temple = db.Table('priest_temple',
-                         db.Column('priest_id', db.Integer(), db.ForeignKey(
-                             'priest.id'), primary_key=True),
-                         db.Column('temple_id', db.Integer(), db.ForeignKey(
-                             'temple.id'), primary_key=True)
-                         )
+    # table many-to-many
+    priest_temple = db.Table('priest_temple',
+                             db.Column('priest_id', db.Integer(), db.ForeignKey('priest.id'), primary_key=True),
+                             db.Column('temple_id', db.Integer(), db.ForeignKey('temple.id'), primary_key=True))
 
 
 class Priest(db.Model):
@@ -212,8 +199,7 @@ class Priest(db.Model):
 
     # atributes
     __tablename__ = 'priest'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     title = db.Column(db.Text)
     first_name = db.Column(db.Text)
     last_name = db.Column(db.Text)
@@ -221,8 +207,7 @@ class Priest(db.Model):
     price = db.Column(db.Integer)
 
     def __repr__(self):
-        return 'Kapłan {}, {}' % (format(self.first_name), format(self.last_name) )
-       
+        return 'Kapłan {}, {}' % (format(self.first_name), format(self.last_name))
 
 
 class Temple(db.Model):
@@ -230,8 +215,7 @@ class Temple(db.Model):
 
     # atributes
     __tablename__ = 'temple'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     voivodeship = db.Column(db.Text)
     county = db.Column(db.Text)
     locality = db.Column(db.Text)
@@ -240,5 +224,5 @@ class Temple(db.Model):
     rank = db.Column(db.Text)
 
     def __repr__(self):
-        return 'Swiątynia wyznania {}, umiejscowiona w {}, {}, {}' % (format(self.religion), format(self.voivodeship), format(self.county), format(self.locality))
-       
+        return 'Swiątynia wyznania {}, umiejscowiona w {}, {}, {}' % (
+            format(self.religion), format(self.voivodeship), format(self.county), format(self.locality))
