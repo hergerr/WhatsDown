fcd171ccb23011ceb82f8dc410955f699297b76d
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-11-20 18:51:03 +0100

    Added more stats

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for
 from whatsdown import app, db
 from collections import Counter
 from whatsdown.forms import LoginForm, RegisterAdminForm, RegisterUserForm
-from whatsdown.models import Administrator, User, Priest, Buried, Outfit
+from whatsdown.models import Administrator, User, Priest, Buried, Outfit, Container, Funeral
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_required, login_user, logout_user
 
@@ -49,9 +49,17 @@ def statistics():
     counter = Counter([brand.brand for brand in outfits])
     most_popular_outfit_brand = counter.most_common(1)[0]
 
+    containers = Container.query.all()
+    counter = Counter([container.type_of_container for container in containers])
+    most_popular_container_type = counter.most_common(1)[0]
+
+    most_expensive_funeal = Funeral.query.order_by(Funeral.total_price)[-1]
+
     return render_template('statistics.html', most_exclusive_priest=most_exclusive_priest,
                            avg_fun_house_price=avg_fun_house_price, buried_number=buried_number,
-                           most_popular_outfit_brand=most_popular_outfit_brand)
+                           most_popular_outfit_brand=most_popular_outfit_brand,
+                           most_popular_container_type=most_popular_container_type,
+                           most_expensive_funeal=most_expensive_funeal)
 
 
 @app.route('/logout')
