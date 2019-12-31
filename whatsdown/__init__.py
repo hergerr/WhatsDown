4e7778368233a163b0c14a3ca8113c6c79008f13
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-12-31 23:16:26 +0100

    Removed bootstrap, set templetes structure

--- whatsdown/__init__.py
+++ whatsdown/__init__.py
@@ -3,13 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from whatsdown.config import name, db_name, password
-from flask_bootstrap import Bootstrap
 from flask_whooshee import Whooshee
 from datetime import timedelta
 
 
 app = Flask(__name__)
-Bootstrap(app)
 
 # db and admin config
 app.secret_key = 'w4lepsze'
@@ -23,7 +21,6 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
 db = SQLAlchemy(app)
 Migrate(app, db)
 admin = Admin(app)
-# admin.add_link(flask_admin.MenuLink(name='Logout', category='', url="/logout"))
 
 whooshee = Whooshee(app)
 whooshee.reindex()
