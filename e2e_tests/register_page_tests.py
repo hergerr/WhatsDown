23c9995d77657aceba91767c5b63b7fdc920b4aa
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-01-23 22:59:01 +0100

    Minor tests enhancement

--- e2e_tests/register_page_tests.py
+++ e2e_tests/register_page_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 
@@ -8,46 +9,58 @@ class RegistrationPage(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_user_registration(self):
-        driver = self.driver
-        driver.get("https://whatsdown.tscode.net/signup-user")
-        elem = driver.find_element_by_name("username")
+    def test01_user_registration(self):
+        self.driver.get("https://whatsdown.tscode.net/signup-user")
+        elem = self.driver.find_element_by_name("username")
         elem.send_keys("new_user")
-        elem = driver.find_element_by_name("password")
+        elem = self.driver.find_element_by_name("password")
         elem.send_keys("new_password")
-        elem = driver.find_element_by_name("confirm")
+        elem = self.driver.find_element_by_name("confirm")
         elem.send_keys("new_password")
-        elem = driver.find_element_by_name("name")
+        elem = self.driver.find_element_by_name("name")
         elem.send_keys("New User")
-        elem = driver.find_element_by_name("county")
+        elem = self.driver.find_element_by_name("county")
         elem.send_keys("Wrocław")
-        elem = driver.find_element_by_name("locality")
+        elem = self.driver.find_element_by_name("locality")
         elem.send_keys("Wrocław")
-        elem = driver.find_element_by_name("phone")
+        elem = self.driver.find_element_by_name("phone")
         elem.send_keys("132 123 123")
-        elem = driver.find_element_by_name("price")
+        elem = self.driver.find_element_by_name("price")
         elem.send_keys("400")
-        elem = driver.find_element_by_class_name("button")
+        elem = self.driver.find_element_by_class_name("button")
         elem.click()
-        assert "New funeral agency created" in driver.page_source
+        sleep(2)
+        assert "New funeral agency created" in self.driver.page_source
 
-    def test_user_removal(self):
-        driver = self.driver
-        driver.get("https://whatsdown.tscode.net/login")
-        elem = driver.find_element_by_name("username")
-        elem.send_keys("admin")
-        elem = driver.find_element_by_name("password")
-        elem.send_keys("admin")
-        elem = driver.find_element_by_class_name("button")
+    def test02_login_as_new_user(self):
+        self.driver.get("https://whatsdown.tscode.net/login")
+        # elem = self.driver.find_element_by_xpath("//div[@class='navbar-list']//a[contains(text(),'Login')]")
+        # elem.click()
+        elem = self.driver.find_element_by_name("username")
+        elem.send_keys("new_user")
+        elem = self.driver.find_element_by_name("password")
+        elem.send_keys("new_password")
+        elem = self.driver.find_element_by_class_name("button")
         elem.click()
-        funeral_home_menu = driver.find_element_by_xpath("//*[contains(text(),'Funeral Home')]")
+        sleep(2)
+        assert "Welcome New User" in self.driver.page_source
+
+    def test03_user_removal(self):
+        self.driver.get("https://whatsdown.tscode.net/login")
+        elem = self.driver.find_element_by_name("username")
+        elem.send_keys("admin")
+        elem = self.driver.find_element_by_name("password")
+        elem.send_keys("admin")
+        elem = self.driver.find_element_by_class_name("button")
+        elem.click()
+        funeral_home_menu = self.driver.find_element_by_xpath("//*[contains(text(),'Funeral Home')]")
         funeral_home_menu.click()
-        delete_fun_home_button = driver.find_element_by_xpath("//tbody//tr[last()]//span[@class='fa fa-trash glyphicon icon-trash']")
+        delete_fun_home_button = self.driver.find_element_by_xpath("//tbody//tr[last()]//span[@class='fa fa-trash glyphicon icon-trash']")
         delete_fun_home_button.click()
-        alert = driver.switch_to.alert
+        alert = self.driver.switch_to.alert
         alert.accept()
-        sleep(4)
-        assert "new_user" not in driver.page_source
+        sleep(2)
+        assert "new_user" not in self.driver.page_source
 
     def tearDown(self):
         self.driver.close()
